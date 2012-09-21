25ac9451cb43277690972b2e5d11065e905f950e
visualjazz-ttrinh <ttrinh@visualjazz.com.au>
2012-09-21 17:59:45 +1000

    colour trends 2013 build; structure, 'share' page
    Signed-off-by: visualjazz-ttrinh <ttrinh@visualjazz.com.au>

---
+++ includes/ct13.js
@@ -0,0 +1,31 @@
+/*jslint devel: true, sloppy: true, maxerr: 50, indent: 4 */
+
+var DULUX = DULUX || {};
+
+// Tony Trinh, 21.9.12
+DULUX.ct13 = (function ($) {
+
+	// var youtubeId,
+	// 	$context = $('.vid-list'),
+	// 	initVideo = function () {
+	// 		// video select/play function, controls videoPlayer.js
+	// 		$('.vid-play', $context).click(function(event) {
+	// 			event.preventDefault();
+	// 			if ($(this).hasClass('active')) return;
+	// 			youtubeId = $(this).attr('data-youtubeid');
+	// 			// .attr(attributeName, value) 'sets', instead of attr(attributeName) which only 'gets'
+	// 			$('#videoDiv').attr('data-youtubeid', youtubeId);
+	// 			HFE.videoPlayer().init('#videoDiv');
+	// 			$('.vid-play', $context).removeClass('active');
+	// 			$(this).addClass('active');
+	// 		});
+	// 	}
+
+	// public functions
+	return {
+		init: function () {
+
+		}
+	};
+
+}(jQuery));
