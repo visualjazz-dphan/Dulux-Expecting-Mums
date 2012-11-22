9242215cbb2e3c13e288d400cc8f4ad2025f398e
visualjazz-dphan <dphan@visualjazz.com.au>
2012-11-22 15:08:33 +1100

    Update the vote percentage Mini Makeover

--- landing-mini-makeover/scripts/vj-global.js
+++ landing-mini-makeover/scripts/vj-global.js
@@ -5,9 +5,12 @@ MINIMAKER.presentation = (function () {
 		minimakerVote = {
 			 init: function (){
 				this.voteColor();
+				this.updateCalculation();
+				this.cookiesPercentage();
 			},			
 			voteColor : function(){				
-				var resultPosition = $(".results").position().top;	
+				var _self = this;
+					resultPosition = $(".results").position().top;	
 
 				$(".dulux-color-section a").bind("click", function(e){
 					
@@ -17,29 +20,52 @@ MINIMAKER.presentation = (function () {
 					
 					var colorSelection = $(this).attr("href");						
 					
-					$(colorSelection).hide();	
-									
-					var currentVote = $(colorSelection).text().split("%");
-						
-						totalVote = parseFloat(currentVote) + 0.1;					
-						$(colorSelection).fadeIn(700).html(totalVote.toFixed(1) + "%");
-						e.preventDefault();
+					$(colorSelection).find("input").val( parseInt($(colorSelection).find("input").val()) + 1 );
 					
 					$(".dulux-color-section a").removeAttr("href");
 					
 					var currentRecord = $(".results li").text().replace( /\%/ig , ',');
 					
 					$.cookie("recordColor", currentRecord.substr( 0,  currentRecord.length - 1), {expires: 365});
+					_self.updateCalculation();
+					_self.cookiesPercentage();
 				});	
+			}, 
+			updateCalculation : function(){
+				$.fn.sumValues = function() {
+					var sum = 0;
+					this.each(function() {
+						if ( $(this).is(':input') ) {
+							var val = $(this).val();
+						} else {
+							var val = $(this).text();
+						}
+						sum += parseFloat( ('0' + val).replace(/[^0-9-\.]/g, ''), 10);
+					});
+					return sum;
+				}; 
 				
-				try {
-					var votes = $.cookie("recordColor").split(',');
-					$(".results li").each(function(e) {
-                        $(this).text( votes[e] + '%' );
-                    });
-				} catch(e) {
+				function percentage(price,sum)
+				{
+					var p = 0;
+					p = parseFloat(price/sum*100).toFixed(1);
+					if(isNaN(p)) p = '';
+					return p;
 				}
-
+				
+				
+				$('.results li span').each(function(){
+					$('.results li span').hide();
+					$('.results li span').fadeIn();
+					$(this).html(percentage($(this).prev("input").val(),$('input.price').sumValues()) + "%");
+					if($(this).prev("input").val() == 0){
+						$(this).html("0.0%");	
+					}
+				});
+						
+			}, 
+			cookiesPercentage : function(){
+				
 			}
 		}
 		
