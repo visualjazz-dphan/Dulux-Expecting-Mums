7c4a8feefe9ddd74c164ed00062c76540864bdf1
visualjazz-ttrinh <ttrinh@visualjazz.com.au>
2013-02-05 18:22:30 +1100

    sample pots build: page + carousel
    Signed-off-by: visualjazz-ttrinh <ttrinh@visualjazz.com.au>

--- sample-pots/scripts/vj-global.js
+++ sample-pots/scripts/vj-global.js
@@ -2,14 +2,36 @@ var MYPROJECT = MYPROJECT || {};
 
 MYPROJECT.samplePots = (function () {
 		
-	var someFunction = function () {
-		//..
+	var colourCarousel = function () {
+
+		var slideColor, slideClass;
+
+		$('#sp-carousel-slides')
+			.cycle({ 
+			    speed:       500, 
+			    timeout:     0, 
+			    pager:      '#sp-carousel-nav', 
+			    pagerEvent: 'click', 
+			    pauseOnPagerHover: true,
+			    next: '#sp-carousel-next', 
+    			prev: '#sp-carousel-prev',
+
+    			// callback fn that creates a thumbnail to use as pager anchor 
+			    pagerAnchorBuilder: function(idx, slide) { 
+
+			    	slideColor = $(slide).data('cname');
+					slideClass = $(slide).data('class');
+					
+			        return '<li class="' +slideClass +'"><a href="#">' +slideColor +'</a></li>'; 
+			    } 
+			});
+
 	}
 		
 	// public functions
 	return {
 		init: function () {
-			//someFunction();
+			colourCarousel();
 		}
 	};
 
